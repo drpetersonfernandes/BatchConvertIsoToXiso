233a0ffbf1317badedb74b3fbf041e0469da6262
Peterson Fernandes <doutorpeterson@gmail.com>
2026-01-27 23:26:00 -0300

    Refactor `XDVDFS` directory traversal to use an iterative approach, improving maintainability and preventing stack overflow risks.

--- BatchConvertIsoToXiso/Services/XisoServices/XDVDFS/XDVDFS.cs
+++ BatchConvertIsoToXiso/Services/XisoServices/XDVDFS/XDVDFS.cs
@@ -14,90 +14,109 @@ internal static class Xdvdfs
     private const long XisoHeaderOffset = 0x10000;
     public static readonly byte[] Magic = "XBOX_DVD_LAYOUT_TOOL_SIG"u8.ToArray();
 
-    // Traverse file tree to get all valid data sectors in XISO
-    private static void GetValidSectors(FileStream isoFs, long isoOffset, List<uint> validSectors,
-        long rootOffset, uint rootSize, long childOffset, bool quiet, bool skipSystemUpdate,
-        HashSet<long> visited)
+    private struct DirectoryWorkItem
     {
-        if (childOffset >= rootSize) return;
+        public long RootOffset;
+        public uint RootSize;
+        public long ChildOffset;
+    }
 
-        var cur = isoOffset + rootOffset + childOffset;
+    // Traverse file tree to get all valid data sectors in XISO using an iterative approach
+    private static void GetValidSectors(FileStream isoFs, long isoOffset, List<uint> validSectors,
+        long rootOffset, uint rootSize, bool quiet, bool skipSystemUpdate, HashSet<long> visited)
+    {
+        var stack = new Stack<DirectoryWorkItem>();
+        stack.Push(new DirectoryWorkItem { RootOffset = rootOffset, RootSize = rootSize, ChildOffset = 0 });
 
-        // Cycle detection
-        if (!visited.Add(cur)) return;
-
-        // Add the directory table sectors themselves as valid, but only once per table.
-        // We only need to do this when childOffset == 0 (first entry in the table).
-        if (childOffset == 0)
+        while (stack.Count > 0)
         {
-            var curOffset = cur / Utils.SectorSize;
-            var curSize = (rootSize + Utils.SectorSize - 1) / Utils.SectorSize;
-            for (var i = curOffset; i < curOffset + curSize; i++)
-                validSectors.Add((uint)i);
-        }
+            var item = stack.Pop();
 
-        isoFs.Seek(cur, SeekOrigin.Begin);
+            if (item.ChildOffset >= item.RootSize) continue;
 
-        // Read LeftSubTree
-        var leftChildOffset = Utils.ReadUShort(isoFs);
+            var cur = isoOffset + item.RootOffset + item.ChildOffset;
 
-        // Check for empty directory (at start offset with 0xFFFF)
-        if (leftChildOffset == 0xFFFF && childOffset == 0)
-        {
-            // Empty directory - no entries to process
-            return;
-        }
+            // Cycle detection
+            if (!visited.Add(cur)) continue;
 
-        // Continue reading the rest of the entry
-        var rightChildOffset = Utils.ReadUShort(isoFs);
-        var entryOffsetRaw = Utils.ReadUInt(isoFs);
-        var entryOffset = (long)entryOffsetRaw * Utils.SectorSize;
-        var entrySize = Utils.ReadUInt(isoFs);
-        var attributes = (byte)isoFs.ReadByte();
-        var nameLength = (byte)isoFs.ReadByte();
-
-        var fileName = "";
-        if (nameLength > 0)
-        {
-            var nameBuffer = new byte[nameLength];
-            isoFs.ReadExactly(nameBuffer, 0, nameLength);
-            fileName = Encoding.ASCII.GetString(nameBuffer);
-        }
-
-        var isDirectory = (attributes & 0x10) != 0;
-
-        // Traverse Left Child (only if valid offset)
-        if (leftChildOffset != 0xFFFF && leftChildOffset != 0 && childOffset != leftChildOffset * 4)
-        {
-            GetValidSectors(isoFs, isoOffset, validSectors, rootOffset, rootSize,
-                (long)leftChildOffset * 4, quiet, skipSystemUpdate, visited);
-        }
-
-        // Process Current Entry
-        if (isDirectory)
-        {
-            // Skip contents of $SystemUpdate if requested
-            if (skipSystemUpdate && fileName.Equals("$SystemUpdate", StringComparison.OrdinalIgnoreCase))
+            // Add the directory table sectors themselves as valid, but only once per table.
+            if (item.ChildOffset == 0)
             {
-                if (!quiet) Debug.WriteLine("Skipping $SystemUpdate directory contents.");
+                var curOffset = cur / Utils.SectorSize;
+                var curSize = (item.RootSize + Utils.SectorSize - 1) / Utils.SectorSize;
+                for (var i = curOffset; i < curOffset + curSize; i++)
+                    validSectors.Add((uint)i);
+            }
+
+            isoFs.Seek(cur, SeekOrigin.Begin);
+
+            // Read LeftSubTree
+            var leftChildOffset = Utils.ReadUShort(isoFs);
+
+            // Check for empty directory (at start offset with 0xFFFF)
+            if (leftChildOffset == 0xFFFF && item.ChildOffset == 0)
+            {
+                continue;
+            }
+
+            // Continue reading the rest of the entry
+            var rightChildOffset = Utils.ReadUShort(isoFs);
+            var entryOffsetRaw = Utils.ReadUInt(isoFs);
+            var entryOffset = (long)entryOffsetRaw * Utils.SectorSize;
+            var entrySize = Utils.ReadUInt(isoFs);
+            var attributes = (byte)isoFs.ReadByte();
+            var nameLength = (byte)isoFs.ReadByte();
+
+            var fileName = "";
+            if (nameLength > 0)
+            {
+                var nameBuffer = new byte[nameLength];
+                isoFs.ReadExactly(nameBuffer, 0, nameLength);
+                fileName = Encoding.ASCII.GetString(nameBuffer);
+            }
+
+            var isDirectory = (attributes & 0x10) != 0;
+
+            // Push Right Child to stack
+            if (rightChildOffset != 0xFFFF && rightChildOffset != 0)
+            {
+                stack.Push(item with { ChildOffset = (long)rightChildOffset * 4 });
+            }
+
+            // Process Current Entry
+            if (isDirectory)
+            {
+                // Skip contents of $SystemUpdate if requested
+                if (skipSystemUpdate && fileName.Equals("$SystemUpdate", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (!quiet) Debug.WriteLine("Skipping $SystemUpdate directory contents.");
+                }
+                else if (entryOffsetRaw > 0)
+                {
+                    // Push Subdirectory to stack
+                    stack.Push(new DirectoryWorkItem
+                    {
+                        RootOffset = entryOffset,
+                        RootSize = entrySize,
+                        ChildOffset = 0
+                    });
+                }
             }
             else if (entryOffsetRaw > 0)
             {
-                GetValidSectors(isoFs, isoOffset, validSectors, entryOffset, entrySize, 0, quiet, skipSystemUpdate, visited);
+                // Add file data sectors
+                var fileOffset = (isoOffset + entryOffset) / Utils.SectorSize;
+                var fileSize = (entrySize + Utils.SectorSize - 1) / Utils.SectorSize;
+                for (var i = fileOffset; i < fileOffset + fileSize; i++)
+                    validSectors.Add((uint)i);
+            }
+
+            // Push Left Child to stack
+            if (leftChildOffset != 0xFFFF && leftChildOffset != 0 && item.ChildOffset != leftChildOffset * 4)
+            {
+                stack.Push(item with { ChildOffset = (long)leftChildOffset * 4 });
             }
         }
-        else if (entryOffsetRaw > 0)
-        {
-            // Add file data sectors
-            var fileOffset = (isoOffset + entryOffset) / Utils.SectorSize;
-            var fileSize = (entrySize + Utils.SectorSize - 1) / Utils.SectorSize;
-            for (var i = fileOffset; i < fileOffset + fileSize; i++)
-                validSectors.Add((uint)i);
-        }
-
-        // Traverse Right Child (only if valid offset)
-        if (rightChildOffset != 0xFFFF && rightChildOffset != 0)
-            GetValidSectors(isoFs, isoOffset, validSectors, rootOffset, rootSize, (long)rightChildOffset * 4, quiet, skipSystemUpdate, visited);
     }
 
     public static List<(uint Start, uint End)> GetXisoRanges(FileStream isoFs, long offset, bool quiet, bool skipSystemUpdate)
@@ -115,7 +134,7 @@ internal static class Xdvdfs
         var rootSize = Utils.ReadUInt(isoFs);
 
         var visited = new HashSet<long>();
-        GetValidSectors(isoFs, offset, validSectors, (long)rootOffset * Utils.SectorSize, rootSize, 0, quiet, skipSystemUpdate, visited);
+        GetValidSectors(isoFs, offset, validSectors, (long)rootOffset * Utils.SectorSize, rootSize, quiet, skipSystemUpdate, visited);
 
         var ranges = new List<(uint, uint)>();
         if (validSectors.Count == 0) return ranges;
